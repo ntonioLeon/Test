b6b3f357298be9de36315ab7d52015c74557f10c
yuripastorovic <jorgepastor12@gmail.com>
2023-12-29 02:28:03 +0100

    Commit 10 # Proyectiles Player

---
+++ Assets/2. Scripts/PlayerSubWeapon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSubWeapon : MonoBehaviour
+{
+    public int costeArma;
+    public GameObject proyectil;
+    public float velocidadProyectil;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UsoSubWeapon();
+    }
+
+    public void UsoSubWeapon() 
+    {
+        if (Input.GetButtonDown("Fire2") && costeArma <= SubItems.instance.total) 
+        {
+            SubItems.instance.Abastecimiento(-costeArma);
+            GameObject disparo = Instantiate(proyectil, transform.position, Quaternion.Euler(0,0,0));
+
+            if (transform.localScale.x < 0)
+            {
+                disparo.GetComponent<Rigidbody2D>().AddForce(new Vector2(-velocidadProyectil, 0f), ForceMode2D.Force);
+                disparo.transform.localScale = new Vector2(-1, -1);
+            }
+            else 
+            {
+                disparo.GetComponent<Rigidbody2D>().AddForce(new Vector2(velocidadProyectil, 0f), ForceMode2D.Force);
+                disparo.transform.localScale = new Vector2(1, 1);
+            }
+        }
+    }
+}
